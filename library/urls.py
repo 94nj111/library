34ddddf97748c4f5f1f882d0059ceb49b51197e1
Dmytro <94nj111@gmail.com>
2024-11-17 15:27:05 +0200

    templates fix

--- library/urls.py
+++ library/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+from debug_toolbar import urls as debug_toolbar_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("catalog/", include("catalog.urls", namespace="catalog"))
-] + debug_toolbar_urls()
+]
+
+if settings.DEBUG:  # Подключаем только в режиме отладки
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar_urls)),
+    ]
